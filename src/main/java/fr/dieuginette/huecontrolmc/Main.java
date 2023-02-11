a3317c0322d04aabbd93e05ab00d25cdbe539837
nathanblanchet@otmail.com <nathanblanchet@hotmail.com>
2023-02-11 15:31:52 +0100

    Création de la base du plugin

---
+++ src/main/java/fr/dieuginette/huecontrolmc/Main.java
@@ -0,0 +1,17 @@
+package fr.dieuginette.huecontrolmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+      System.out.println("Bienvenue dans le plugin");
+
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Merci d'avoir utiliser le plugin");
+    }
+}
